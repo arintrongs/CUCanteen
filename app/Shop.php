fb362cec54c56ae2d14944fce62576737c5c857c
tan14007 <panukorn1400@naver.com>
2017-08-12 19:05:48 +0700

    Addidng some methods

---
+++ app/Shop.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Shop extends Model
+{
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'tb_shop';
+
+    /**
+     * The connection name for the model.
+     *
+     * @var string
+     */
+    protected $connection = 'mysql';
+
+    /**
+     * Get all shop picture (For making Background)
+     * @var array
+     */
+    public function get_all_picture(){
+        $shops = $this->all()->get();
+        
+    }
+
+    public function get_shop($shop_id = 0)
+    {
+    	$shop = $this->where('shop_id', $shop_id)->first();;
+
+        $data = array(
+        	'img' => $shop->shop_img,
+        	'name' => $shop->shop_name,
+            'description' => $shop->shop_description,
+        );
+		return $data;
+    }
+}
--- config/database.php
+++ config/database.php
@@ -57,7 +57,7 @@ return [
             'host'      => env('DB_HOST', 'localhost'),
             'database'  => env('DB_DATABASE', 'db_canteen'),
             'username'  => env('DB_USERNAME', 'root'),
-            'password'  => env('DB_PASSWORD', 'payuheekatymolee'),
+            'password'  => env('DB_PASSWORD', ''),
             'charset'   => 'utf8',
             'collation' => 'utf8_unicode_ci',
             'prefix'    => '',
