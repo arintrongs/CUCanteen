938c139dc7ede87d81fb80292b47b44cae1b3176
Akarapon Watcharapalakorn <akaraponwatcharapalakorn@Akarapons-MacBook-Pro.local>
2017-08-22 13:37:52 +0700

    Committer: tan14007 <tan14007@tan14007.local> Move models back to where it belonged to/ commit before upgrading to laravel 5.4
    move model to where it belonged to. Commit before upgrading laravel to 5.4

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -3,6 +3,12 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Auth\Authenticatable;
+use Illuminate\Auth\Passwords\CanResetPassword;
+use Illuminate\Foundation\Auth\Access\Authorizable;
+use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
+use Illuminate\Contracts\Auth\Access\Authorizable as AuthorizableContract;
+use Illuminate\Contracts\Auth\CanResetPassword as CanResetPasswordContract;
 use App\User;
 
 class Comment extends Model{
@@ -11,7 +17,7 @@ class Comment extends Model{
      *
      * @var string
      */
-    private $table = 'tb_comment';
+    protected $table = 'tb_comment';
 
     /**
      * define custom timestamp column
@@ -25,26 +31,26 @@ class Comment extends Model{
      *
      * @var int
      */
-    private $primaryKey = 'comment_id'
+    protected $primaryKey = 'comment_id'
 
     /**
      * The connection name for the model.
      *
      * @var string
      */
-    private $connection = 'mysql';
+    protected $connection = 'mysql';
 
     /**
      * The connection name for the model.
      *
      * @var array of comment if succeed, false returned if there is an error
      */
-    public function getCommentShop($shop_id = 0,$count = 10){
+    public function getCommentShop($shop_id = 0,$count = 1){
     	if($shop_id!=0){
             $comments = self::where('shop_id', $shop_id)
-                ->leftJoin('user', 'tb_comment.user_id', '=', 'tb_user_profile.user_id')
-                ->select('tb_comment.*', 'tb_user_profile.dispname', 'tb_user_profile.name')
-                ->orderBy('tb_comment.comment_rating', 'desc')
+                ->leftJoin('tb_user', 'tb_comment.user_id', '=', 'tb_user.user_id')
+                ->select('tb_comment.*', 'tb_user.user_dispname', 'tb_user.name')
+                ->orderBy('tb_comment.comment_timestamp', 'desc')
                 ->take($count)
                 ->get();
         
@@ -52,7 +58,7 @@ class Comment extends Model{
                 foreach ($comments as $comment) {
                     $data[] = array(
                         'id' => $comment['comment_id'],
-                        'name' => User::where('user_id',$comment['user_id'])->first()->user_dispname,
+                        'name' => $comment['user_dispname'],
                         'comment' => $comment['comment_text'],
                         'rating' => $comment['comment_rating'],
                         'recommendedfood' => $comment['comment_food'],
@@ -76,13 +82,28 @@ class Comment extends Model{
     }
 
     /**
+     * Add comment 
+     *
+     *  
+     * @var bool False on failure
+     */
+    // public function addComment($data = null){
+    //     if($data != null){
+    //         $comment = new self(array(
+    //             'user_id' => DatabaseUserProvider::
+    //             ));
+    //     }
+    //     return False;
+    // }
+
+    /**
      * Delete comment (Return True on succeed, else False)
      *
      * @var bool
      */
     public function deleteComment($comment_id = 0){
         if($comment_id != 0){
-            $comment = self::where('comment_id',$comment_id)->get();
+            $comment = self::where('comment_id',$comment_id)->first();
             if($comment == null) return False;
             $comment -> forceDelete();
             return True;
