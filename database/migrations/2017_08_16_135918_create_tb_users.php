c005ad8090fb52537014d1927274ee0fb22d185e
DESKTOP-M83089R\aqover-pc <aqover@hotmail.com>
2017-08-26 00:06:24 +0700

    Fix foreign key error
    how to create table
    Step 1: drop all table
    - tb_comment
    - tb_food
    - tb_shop
    - tb_user
    - users
    - password_resets
    - migrations
    
    Step 2: create table
    php artisan migrate:install <-- new order migrate, order timestamp where prefix of name migration file.
    php artisan migrate

---
+++ database/migrations/2017_08_16_135918_create_tb_users.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTbUsers extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        // $this->down();
+        Schema::create('tb_user', function (Blueprint $table) {
+            $table->bigIncrements('user_id');
+            $table->string('user_username', 30);
+            $table->string('user_studentid', 15)->comment('Student\'s ID (user after accquired CUNET API)')->nullable();
+            $table->string('user_fbid', 30)->comment('Student\'s Facebook ID (if there is nothing else to do)')->nullable();
+            $table->string('dispname', 60);
+            $table->string('user_hpassword', 60);
+            $table->text('user_session')->comment('in case od usage (can be deleted)');
+            $table->binary('user_disppict')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tb_user');
+    }
+}
