f79ee88918951cc152645e3f53ae092a30f7fa2c
l3lackclevil <l3lackclevil@hotmail.com>
2017-11-10 15:24:43 +0700

    Minify

--- resources/views/canteen/layouts/layout.blade.php
+++ resources/views/canteen/layouts/layout.blade.php
@@ -137,7 +137,6 @@
         <script src="{{ URL :: asset('js/canteen/shop.js') }}"></script>
         <script src="{{ URL :: asset('js/canteen/u.js') }}"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
-        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js" integrity="sha384-h0AbiXch4ZDo7tp9hKZ4TsHbi047NrKGLO3SEJAg45jXxnGIfYzk4Si90RDIqNm1" crossorigin="anonymous"></script>
         <script src="js/bootstrap3-typeahead.js"></script>
         @yield('footer')
     </body>
